de6af9f19097428987c0b02590ea325368af6eaa
Sidharth Mittal <107436833+kidnapshadow-sidharth@users.noreply.github.com>
2024-09-02 10:09:30 -0700

    eth0 ip

---
+++ xfceetho_ip.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+ip=$(ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
+
+if [ "${ip}" != "" ]; then
+  printf "<icon>network-vpn-symbolic</icon>"
+  printf "<txt>${ip}</txt>"
+  printf "<tool>Eth0 IP</tool>"
+else
+  printf "<txt></txt>"
+fi
